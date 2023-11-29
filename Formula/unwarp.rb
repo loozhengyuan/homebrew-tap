6a3f4ef15a18f787cae870d630b9b2c3e529b4d7
Loo Zheng Yuan <loozhengyuan@gmail.com>
2023-11-29 21:19:47 +0800

    chore(fmt): resolve ruby lint/format errors

--- Formula/unwarp.rb
+++ Formula/unwarp.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 class Unwarp < Formula
-    desc "Disable Cloudflare WARP on macOS"
-    homepage "https://github.com/loozhengyuan/unwarp"
-    url "https://github.com/loozhengyuan/unwarp.git",
-        # tag: "v0.1.0"
-        revision: "34578f16bdb1f7ace675770314f6de81b4908a7c"
-    head "https://github.com/loozhengyuan/unwarp.git",
-        branch: "main"
-    version "0.1.0"
-    license "MIT"
-    revision 2
+  desc 'Disable Cloudflare WARP on macOS'
+  homepage 'https://github.com/loozhengyuan/unwarp'
+  url 'https://github.com/loozhengyuan/unwarp.git',
+      # tag: "v0.1.0"
+      revision: '34578f16bdb1f7ace675770314f6de81b4908a7c'
+  head 'https://github.com/loozhengyuan/unwarp.git',
+       branch: 'main'
+  version '0.1.0'
+  license 'MIT'
+  revision 2
 
-    def install
-        bin.install "src/unwarp.sh" => "unwarp"
-    end
+  def install
+    bin.install 'src/unwarp.sh' => 'unwarp'
+  end
 
-    service do
-        run opt_bin/"unwarp"
-        keep_alive always: true
-        environment_variables PATH: std_service_path_env
-        log_path var/"log/unwarp.log"
-        error_log_path var/"log/unwarp.log"
-    end
+  service do
+    run "#{opt_bin}/unwarp"
+    keep_alive always: true
+    environment_variables PATH: std_service_path_env
+    log_path "#{var}log/unwarp.log"
+    error_log_path "#{var}log/unwarp.log"
+  end
 
-    test do
-        assert_match version.to_s, shell_output("#{bin}/unwarp -v 2>&1")
-    end
+  test do
+    assert_match version.to_s, shell_output("#{bin}/unwarp -v 2>&1")
+  end
 end
